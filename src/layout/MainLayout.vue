f1c38d02f3365a9634d1d30c7107f7e71ec5e766
hakrithy <hakrithykim2020@gmail.com>
2025-03-27 14:19:45 +0700

    add

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hi bg-red-500 w-full h-full text-black-500">
+            hi layout mainasdsa
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
