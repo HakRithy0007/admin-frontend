7e67cf5101f05678339d728c72dc759313cd433c
hakrithy <hakrithykim2020@gmail.com>
2025-04-07 15:55:19 +0700

    add

--- src/views/Reviews.vue
+++ src/views/Reviews.vue
@@ -6,7 +6,7 @@
         <input type="text" placeholder="Search reviews..." class="border border-gray-300 roundedpx-4 py-2 w-1/3 focus:ring-2 focus:ring-blue-500 outline-none" />
         <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">+ Add Review</button>
       </div>
-      <!-- Reviews Table -->
+      <!-- Reviews Tables -->
       <div class="overflow-x-auto bg-white shadow-md rounded">
         <table class="min-w-full border-collapse">
           <thead class="bg-gray-200">
