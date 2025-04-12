fa975fb4eadea59c309beb91728e7dbcbf0a6d7e
hakrithy <hakrithykim2020@gmail.com>
2025-04-12 08:41:03 +0700

    add

--- src/utils/common/format_date.ts
+++
@@ -1,9 +0,0 @@
-import dayjs from "dayjs";
-
-export const formatDate = (date: string | Date): string => {
-    return dayjs(date).format('YYYY-MM-DD');
-}
-
-export const formatTime = (dateTime: string | Date): string => {
-    return dayjs(dateTime).format("HH:mm:ss");
-};
